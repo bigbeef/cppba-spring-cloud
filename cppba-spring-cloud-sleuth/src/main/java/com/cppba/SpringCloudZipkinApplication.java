c920d3e00610367e0ea24cda35e02a02d271c38f
winfed <245655812@qq.com>
2017-08-11 17:45:26 +0800

    服务链路追踪

---
+++ cppba-spring-cloud-sleuth/src/main/java/com/cppba/SpringCloudZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.cppba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class SpringCloudZipkinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudZipkinApplication.class, args);
+    }
+}
