c2731ec5bd8cc6634cc32dfc5ac653744453884f
winfed <245655812@qq.com>
2017-09-18 15:27:25 +0800

    config读取配置

--- cppba-spring-cloud-eureka-client/src/main/java/com/cppba/SpringCloudEurekaClientApplication.java
+++ cppba-spring-cloud-eureka-client/src/main/java/com/cppba/SpringCloudEurekaClientApplication.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication
 @RestController
 public class SpringCloudEurekaClientApplication {
 
