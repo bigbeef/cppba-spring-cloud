c473e2e7fcfdf3aac5ea6787687f3b7810c32d92
winfed <245655812@qq.com>
2017-09-18 14:30:28 +0800

    sleuth配置读取

--- cppba-spring-cloud-sleuth/src/main/java/com/cppba/SpringCloudZipkinApplication.java
+++ cppba-spring-cloud-sleuth/src/main/java/com/cppba/SpringCloudZipkinApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import zipkin.server.EnableZipkinServer;
 
-@SpringBootApplication
 @EnableZipkinServer
+@SpringBootApplication
 public class SpringCloudZipkinApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudZipkinApplication.class, args);
