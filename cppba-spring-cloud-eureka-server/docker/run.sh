96910e7873bec463e32ebc8b3dcf5e8988b81b60
winfed <chinesezwf@164.com>
2018-06-04 21:28:36 +0800

    docker test

--- cppba-spring-cloud-eureka-server/docker/run.sh
+++ cppba-spring-cloud-eureka-server/docker/run.sh
@@ -11,3 +11,4 @@ docker rmi winfed/${app_name}
 docker build -t winfed/${app_name} .
 # 启动一个容器
 docker run -d --name ${app_name} -p 8761:8080 winfed/${app_name}
+docker run -d --name ${app_name} -p 8762:8080 winfed/${app_name}
