afb4dd921a42d0cf9955cdfeda51f76e91aa0bde
winfed <chinesezwf@164.com>
2018-06-04 21:51:48 +0800

    docker

--- cppba-spring-cloud-eureka-server/docker/run.sh
+++ cppba-spring-cloud-eureka-server/docker/run.sh
@@ -2,7 +2,7 @@
 
 app_name="cppba-spring-cloud-eureka-server"
 port1="8761"
-port2="8762"
+# port2="8762"
 
 # (确保已经登陆docker)
 # 停止并移除服务
@@ -13,4 +13,4 @@ docker rmi winfed/${app_name}
 docker build -t winfed/${app_name} .
 # 启动一个容器
 docker run -d --name ${app_name}-${port1} -p ${port1}:8080 winfed/${app_name}
-docker run -d --name ${app_name}-${port2} -p ${port2}:8080 winfed/${app_name}
+# docker run -d --name ${app_name}-${port2} -p ${port2}:8080 winfed/${app_name}
