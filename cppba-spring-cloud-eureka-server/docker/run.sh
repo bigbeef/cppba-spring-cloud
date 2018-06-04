e71b9760332e1c220705ce226d9fd11163913b7e
winfed <chinesezwf@164.com>
2018-06-04 21:40:24 +0800

    docker test

--- cppba-spring-cloud-eureka-server/docker/run.sh
+++ cppba-spring-cloud-eureka-server/docker/run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 app_name="cppba-spring-cloud-eureka-server"
+port1="8761"
+port2="8762"
 
 # (确保已经登陆docker)
 # 停止并移除服务
-docker rm -f ${app_name}
+docker rm -f $(docker ps -a -f "name=${app_name}")
 # 移除镜像
 docker rmi winfed/${app_name}
 # 构建镜像
 docker build -t winfed/${app_name} .
 # 启动一个容器
-docker run -d --name ${app_name} -p 8761:8080 winfed/${app_name}
-docker run -d --name ${app_name} -p 8762:8080 winfed/${app_name}
+docker run -d --name ${app_name}-${port1} -p ${port1}:8080 winfed/${app_name}
+docker run -d --name ${app_name}-${port2} -p ${port2}:8080 winfed/${app_name}
