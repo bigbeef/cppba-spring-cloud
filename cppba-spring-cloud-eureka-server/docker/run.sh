1c7e44324d0813bb0147456648edab52cc492f29
winfed <chinesezwf@164.com>
2018-06-05 15:37:11 +0800

    docker test

--- cppba-spring-cloud-eureka-server/docker/run.sh
+++ cppba-spring-cloud-eureka-server/docker/run.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+# echo "java -jar app.jar -Denv=pro " >> docker-entrypoint.sh
+
 app_name="cppba-spring-cloud-eureka-server"
 port1="8761"
 # port2="8762"
 
 # (确保已经登陆docker)
 # 停止并移除服务
-docker rm -f $(docker ps -a -f "name=${app_name}" -q)
+docker rm -f $(docker ps -a -f "name=cppba-spring-cloud-eureka-server" -q)
 # 移除镜像
 docker rmi winfed/${app_name}
 # 构建镜像
-docker build -t winfed/${app_name} .
+docker build -t winfed/${app_name} /code
 # 启动一个容器
-docker run -d --name ${app_name}-${port1} -p ${port1}:8080 --entrypoint="java -jar -Denv=pro app.jar" winfed/${app_name}
-# docker run -d --name ${app_name}-${port2} -p ${port2}:8080 --entrypoint="java -jar -Denv=pro app.jar" winfed/${app_name}
+docker run -d --name ${app_name}-${port1} -p ${port1}:8080 winfed/${app_name}
